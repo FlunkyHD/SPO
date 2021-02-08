74bd4a8bed1df293950947a36eb426738f17a8bc
Morten Jørgensen <mortenj2@hotmail.dk>
2021-02-08 14:10:22 +0100

    HEJ

--- SPO/Lecture 0/src/com/company/Main.java
+++ SPO/Lecture 0/src/com/company/Main.java
@@ -48,7 +48,6 @@ public class Main {
                 listen.add(input.charAt(i));
             }
         }
-
         return listen;
     }
 
--- SPO/Lecture 0/src/com/company/Node.java
+++ SPO/Lecture 0/src/com/company/Node.java
@@ -14,4 +14,5 @@ public class Node {
     public String value;
     public Node left;
     public Node right;
+
 }
